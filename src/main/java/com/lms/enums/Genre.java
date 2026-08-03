44658c0bf46a334378361ba576b009a29eb43cb2
Syed Asma Saeed <syedasmasaeed21@gmail.com>
2026-08-03 20:54:16 +0530

    Initial Project Breakdown

---
+++ src/main/java/com/lms/enums/Genre.java
@@ -0,0 +1,24 @@
+package com.lms.enums;
+
+public class Genre{
+    public enum genre{
+        FICTION("Fiction"),
+        NON_FICTION("Non_Fiction"),
+        SCIENCE("Science"),
+        TECHNOLOGY("Technology"),
+        HISTORY("History"),
+        BIOGRAPHY("Biography"),
+        MATHEMATICS("Mathematics"),
+        PHILOSOPHY("Philosophy");
+
+        private final String displayName;
+
+        genre(String displayName){
+            this.displayName = displayName;
+        }
+
+        public String getDisplayName(){
+            return this.displayName;
+        }
+    }
+}
